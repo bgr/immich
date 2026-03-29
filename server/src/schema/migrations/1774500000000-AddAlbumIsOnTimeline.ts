dc44e11ef70bd4746230ff800c7e18f37f2667d1
bgr <bgr.gyk@gmail.com>
2026-03-29 19:30:38 +0200

    fix: rename migration to sort after v2.6.3 upstream migrations

--- server/src/schema/migrations/1774500000000-AddAlbumIsOnTimeline.ts
+++ server/src/schema/migrations/1774500000000-AddAlbumIsOnTimeline.ts
@@ -1,7 +1,7 @@
 import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await sql`ALTER TABLE "album" ADD "isOnTimeline" boolean NOT NULL DEFAULT true`.execute(db);
+  await sql`ALTER TABLE "album" ADD COLUMN IF NOT EXISTS "isOnTimeline" boolean NOT NULL DEFAULT true`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
